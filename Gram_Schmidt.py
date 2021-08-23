9ee6a95de8b8aa87ebd56f6154cdde99344530b4
Abdul-Rahman Ibrahim <89408640+Abdul-Rahman-Ibrahim@users.noreply.github.com>
2021-08-23 20:29:50 +0300

    Update Gram_Schmidt.py

--- Gram_Schmidt.py
+++ Gram_Schmidt.py
@@ -1,28 +1,25 @@
-import numpy
-
-def gs_cofficient(v1, v2):
-    return numpy.dot(v2, v1) / numpy.dot(v1, v1)
-
-def multiply(cofficient, v):
-    return map((lambda x : x * cofficient), v)
-
 def proj(v1, v2):
-    return multiply(gs_cofficient(v1, v2) , v1)
+    """Returns the projection of v2 onto v1"""
+    coef = np.dot(v2,v1)/np.dot(v1,v1)
+    return np.dot(coef, v1)
 
-def gs(X):
-    Y = []
-    for i in range(len(X)):
-        temp_vec = X[i]
-        for inY in Y :
-            proj_vec = proj(inY, X[i])
-            #print "i =", i, ", projection vector =", proj_vec
-            temp_vec = map(lambda x, y : x - y, temp_vec, proj_vec)
-            #print "i =", i, ", temporary vector =", temp_vec
-        Y.append(temp_vec)
-    return Y
-
-test = numpy.array([[3.0, 1.0], [2.0, 2.0]])
-test2 = numpy.array([[1.0, 1.0, 0.0], [1.0, 3.0, 1.0], [2.0, -1.0, 1.0]])
-
-print numpy.array(gs(test))
-print numpy.array(gs(test2))
+def gramSchmidt(A):
+    """Takes Transpose of a matrix and Returns orthogonal bases of that matrix"""
+    orthVectors = []
+    j = 1
+    for row in A:
+        orthVectors.append(row)
+        if len(orthVectors) == 1:
+            continue
+        pro = 0
+        for i in range(len(orthVectors)-1):
+            pro = np.add(pro, proj(orthVectors[i], row))
+            orthVectors[j] = row - pro
+            j += 1
+    #Converting entries with decimals to fraction
+    orthVectors = sp.Matrix(np.array(orthVectors).T)
+    for i, val in enumerate(orthVectors):
+        frac = Fraction(str(val)).limit_denominator()
+        orthVectors[i] = frac
+        
+    return orthVectors"
